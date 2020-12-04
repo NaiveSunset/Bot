c35fcb41022585738320fbd15fc8af5920e08d3b
Salx <ww.blackknight@gmail.com>
2020-12-04 23:07:38 +0500

    step_2

---
+++ step_00.py
@@ -0,0 +1,10 @@
+# PEP-8
+# snake case
+my_var = 5  # instance
+print(my_var, type(my_var), dir(my_var))
+
+my_var = '5'  # instance
+print(my_var, type(my_var), dir(my_var))
+
+my_var = 5.0  # instance
+print(my_var, type(my_var), dir(my_var))
